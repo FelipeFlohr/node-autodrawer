7532f15cff3b0aedc75c095a8238e68ef6f9e822
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-03-02 13:08:51 -0300

    Added a simple logger

--- src/Index.ts
+++ src/Index.ts
@@ -1,4 +1,7 @@
-import { MouseControl } from "./controls/mousecontrol";
+import { MouseControl } from "./controls/MouseControl";
+import { log, LogLevel } from "./logger/Logger";
 
-MouseControl.moveTo(1070, 15)
-MouseControl.leftClick()
+// MouseControl.moveTo(1070, 15)
+// MouseControl.leftClick()
+
+log(LogLevel.FATAL, "A fatal warning")
